87d49585a67b1a4ee45e6786d791bf4bd515d08d
Sara Pineda <spinedal@eafit.edu.co>
2024-02-25 22:27:45 -0500

    logo y nombre

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import PrimerComponente from './components/PrimerComponente';
 import Botones from './components/Botones';
 import BarraDeBusqueda from './components/BarraDeBusqueda';
-import Head from 'next/head';
-
 
 function App() {
   return (
     <>
-      <Head>
-        <link rel="icon" href="Magneto.Logo.jpg" />
-        <title>Magneto Freelance</title>
-      </Head>
     <div >
     </div>
       <div className='mainPage'>
